3c4060f483f0ab3cf4f36df39a2dc444587478da
d-willb <39524812+d-willb@users.noreply.github.com>
2020-04-16 18:01:12 +0200

    Add files via upload

---
+++ js/wb/wb-obs.js
@@ -0,0 +1,67 @@
+;(function($, window, document, undefined) {
+	var $win = $(window);
+	var $doc = $(document);
+	var $sidebar = $('.widget-sidebar');
+	var activeClass = 'active';
+	var scrollTop = $sidebar.offset().top - $('.site-banner').outerHeight();
+	var sidebarTop = $('.breadcrumb-nav').offset().top + $('.breadcrumb-nav').outerHeight() + 32;
+	
+	initAccordions();
+	
+	/**
+	 * Accordions functionality
+	 * @return {[type]} [description]
+	 */
+	function initAccordions() {
+	    var $accordions = $('.js-accordion');
+	    var $accordionsMobile = $('.js-accordion-mobile');
+
+	    var mobileLandscape = window.matchMedia( "(max-width: 767px)" );
+
+	    if( $accordions.length === 0 ) {
+	        return;
+	    }
+
+	    $accordions.each(function(){
+	        var $accordion = $(this);
+	        var $head = $accordion.find(' > .js-accordion-head');
+	        var $body = $accordion.find(' > .js-accordion-body');
+
+	        $head.click(function(e){
+	            e.preventDefault();
+
+	            $accordion.toggleClass('is-collapsed', $body.is(':visible')).siblings().addClass('is-collapsed').find('> .js-accordion-body').slideUp();
+	            $body.slideToggle();
+	        });
+	    });
+	}
+
+	//Tabs
+
+	$('.tabs__nav a').on('click', function(event) {
+		event.preventDefault();
+		
+		var $tabLink = $(this);
+		var $targetTab = $($tabLink.attr('href'));
+
+		$tabLink
+			.parent()
+			.add($targetTab)
+			.addClass(activeClass)
+				.siblings()
+				.removeClass(activeClass);
+	});
+
+	var lastScrollTop = 0;
+
+	$win.on('load scroll', function() {
+
+		if ( $win.scrollTop() > scrollTop && !$sidebar.hasClass('sticky') ) {
+			$sidebar.addClass('sticky');
+			$sidebar.css('top', sidebarTop)
+		} else if ( $win.scrollTop() < scrollTop && $sidebar.hasClass('sticky') ) {
+				$sidebar.removeClass('sticky');
+				$sidebar.css('top', 0)
+		} 
+	});
+})(jQuery, window, document);
